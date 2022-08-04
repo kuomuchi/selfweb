7bcc54716da603437c22bea44fb5b775c42f59f0
kuonuchi <kuokillua1228@gmail.com>
2022-08-04 14:10:45 +0800

    add select function

--- public/js/home.js
+++ public/js/home.js
@@ -3,6 +3,7 @@ console.log("Login")
 console.log(location.origin + '/api/data')
 
 // 抓取所有課程資料
+
 const profileData = new Promise((resolve, reject) => {
   fetch(location.origin+ '/api/data', {method:'GET'}).then( res => {
     resolve(res.json())
@@ -12,9 +13,9 @@ const profileData = new Promise((resolve, reject) => {
   })
 })
 
+
 // 開始抓取、建立資料至前端
-profileData.then(data => {
-  console.log(data)
+const catchData = data => {
   for(let time = 0; time < data.length; time++){
     let profile =  data[time]
     
@@ -39,6 +40,44 @@ profileData.then(data => {
 
     outSidsElm.appendChild(newitem)
   }
-})
+}
+
+//第一次抓取資料
+profileData.then(catchData)
+
+
+// select-self
+// select-input
+
+//查詢
+document.getElementById("input-button").addEventListener("click", () => {
+
+  const selectData = new Promise((resolve, reject) => {
+
+    const keyword = document.getElementById('select-input').value
+    const type = document.getElementById('select-self').value
+
+    fetch(location.origin+ '/api/data?keyword='+keyword+'&type='+type, {method:'GET'}).then(res => {
+      resolve(res.json())
+      
+    }).catch(error =>{
+      console.log('get new data error \n error is :' + error)
+      reject(error)
+      return
+    })
+
+  })
+
+  document.getElementById("page-body").innerHTML = ""
+  selectData.then(catchData)
+
+  //reset
+  document.getElementById("select-input").value = ''
+
+
+
+});
+
+
 
 
--- server/controls/data.js
+++ server/controls/data.js
@@ -5,10 +5,32 @@ const {
 
 const getData = async (req, res) => {
 
-	// console.log(req.query) 獲得後方參數
+	console.log(req.query) //獲得後方參數
 
-	const data = await query("SELECT * FROM learn_process;")
+	//keyword input輸入
+	//type 類型
+
+	let keyword = req.query.keyword
+	let type = req.query.type
+
+	if(!keyword){
+		keyword = "%%"
+	}else{
+		keyword = "%" + keyword + "%"
+	}
+
+	if(!type || type == "總覽"){
+		type = "%%"
+	}else{
+		type = "%" + type + "%"
+	}
+
+	const sql = "SELECT * FROM selfweb.learn_process WHERE (type Like '"+ type +"') AND  (title Like '" + keyword +"') OR (directions like'"+ keyword +"');"
+	const data = await query(sql)
+
 	res.json(data)
+	
+	
 }
 
 const test = async (req, res) => {
