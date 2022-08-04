d59760b8a4a5b3855caed2815231fa794b732589
kuonuchi <kuokillua1228@gmail.com>
2022-08-04 20:03:32 +0800

    add editer function

---
+++ public/js/add.js
@@ -0,0 +1,66 @@
+let selectId = 'new'
+
+
+// 抓取所有課程資料
+const profileData = new Promise((resolve, reject) => {
+  fetch(location.origin+ '/api/data', {method:'GET'}).then( res => {
+    resolve(res.json())
+  }).catch( error =>{
+    console.log('data error')
+    reject(error)
+  })
+})
+
+
+// 開始抓取、建立資料至前端
+const catchData = data => {
+
+  console.log(data)
+  for(let time = 0; time < data.length; time++){
+    let profile =  data[time]
+    
+    //Catch Page-body element
+    const outSidsElm = document.getElementsByClassName('select-box')[0]
+
+    //create data to front-end
+    const newitem = document.createElement('div')
+    newitem.className = 'msg-item'
+
+    //create html
+    newitem.innerHTML ="\
+    <div>" + profile.title +"</div>\
+    <div>" + profile.time_date +"</div>\
+    <div>" + profile.type +"</div>"
+
+    newitem.addEventListener('click', ()=>{
+
+      selectId = profile.id
+
+      let content = profile.directions
+      document.getElementById('title-edit').value = profile.title
+      document.getElementById('time-edit').value = profile.time_date
+      document.getElementById('type-edit').value = profile.type
+
+      content = content.replaceAll('<br>', "\n");
+
+      document.getElementById('directions-edit').value = content
+
+      content = content.replaceAll('\n', "<br>");
+      descriptContent = content
+
+    })
+
+    outSidsElm.appendChild(newitem)
+  }
+}
+
+//第一次抓取資料
+profileData.then(catchData)
+
+document.getElementById('reset').addEventListener('click', () => {
+
+  document.getElementById('title-edit').value = ''
+  document.getElementById('time-edit').value = ''
+  document.getElementById('directions-edit').value = ''
+  
+})
